eceb7a028c0dbab08ef89bf72282f572fb3e7853
unknown <l4ut4r096@gmail.com>
2023-12-10 20:54:40 -0300

    Cambio p vale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -104,7 +104,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'login'
   }
 ];
 
--- src/app/components/popup/popup-dialog/popup-dialog.component.ts
+++ src/app/components/popup/popup-dialog/popup-dialog.component.ts
@@ -42,8 +42,8 @@ export class PopupDialogComponent {
     private loginService: LoginService
   ) {
     this.telefono = this.loginService.getTelefono();
-  this.nuevoPartido = new Partido();
-  this.nuevoPartido.horario = this.horarioEnumValues[0];
+    this.nuevoPartido = new Partido();
+    this.nuevoPartido.horario = this.horarioEnumValues[0];
   }
 
   ngOnInit() {
@@ -56,29 +56,23 @@ export class PopupDialogComponent {
     console.log('Valor de partidoForm:', partidoForm.value);
     const horarioString = partidoForm.value.horario;
     const telefono = this.telefono;
-  
+
     const horario: Horarios | undefined = this.convertirStringAHorario(horarioString);
-  
+
+
     if (horario !== undefined) {
       this.nuevoPartido.horario = horario;
-  
+
       // Verificar si nuevoPartido.dia tiene un valor no nulo
       if (this.nuevoPartido.dia) {
         const formattedDia: string = this.formatDateForApi(this.nuevoPartido.dia);
-  
+
         // Asegúrate de que formattedDia sea una cadena antes de usarlo
         if (typeof formattedDia === 'string') {
           this.partidoService.sacarTurno(this.nuevoPartido.horario, formattedDia, telefono)
             .subscribe(
               result => {
-                Swal.fire({
-                  title: "Ha sacado el turno con exito!",
-                  // text: "That thing is still around?",
-                  icon: "success"
-                });
                 console.log(result);
-                // Recargar los partidos después de agregar uno nuevo
-                // this.obtenerPartidos();
               },
               error => {
                 console.error(error);
@@ -93,12 +87,19 @@ export class PopupDialogComponent {
     } else {
       console.error('Horario no reconocido:', horarioString);
     }
+    
+
+    Swal.fire({
+      title: "Ha sacado el turno con exito!",
+      icon: "success"
+    });
+   
   }
-  
-  
-  
-  
-  
+
+
+
+
+
   // private formatDateForApi(date: Date): string {
   //   return `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
   // }
@@ -111,22 +112,22 @@ export class PopupDialogComponent {
       console.error('La fecha es nula.');
     }
   }
-  
-  
+
+
   private formatDateForApi(date: Date): string {
     const year = date.getFullYear();
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const day = ('0' + date.getDate()).slice(-2);
-  
+
     return `${year}-${month}-${day}`;
   }
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
 
 
 
@@ -158,27 +159,27 @@ export class PopupDialogComponent {
 
   convertirStringAHorario(horarioString: string): Horarios | undefined {
     console.log('Valor de horarioString al entrar a la función:', horarioString);
-  
+
     // Verificar si horarioString es undefined o null
     if (!horarioString) {
       console.error('Horario no reconocido: ', horarioString);
       return undefined;
     }
-  
+
     // Loguear el valor actual de horarioString
     console.log('Valor de horarioString antes de transformación:', horarioString);
-  
+
     // Revertir la transformación realizada en la presentación
     const formattedString = horarioString.replace(' hs', '_HS');
-  
+
     // Loguear el valor después de la transformación
     console.log('Valor de formattedString:', formattedString);
-  
+
     const horarioEnum = this.horarioEnumValues.find((enumValue) => {
       console.log('Comparando:', enumValue.toString(), 'con', formattedString.trim());
       return enumValue.toString() === formattedString.trim();
     });
-  
+
     if (horarioEnum) {
       // Loguear el valor de horarioEnum si se encuentra
       console.log('Valor de horarioEnum encontrado:', horarioEnum.toString());
@@ -188,7 +189,7 @@ export class PopupDialogComponent {
       return undefined;
     }
   }
-  
-  
+
+
 
 }
