3067fe335cadf6a2e4f94b0ae9af4d703bbfd689
Luca Rivera <lucamartinrivera@gmail.com>
2023-12-18 18:26:07 -0300

    sven/pass

--- src/app/components/login-encargado/login-encargado.component.ts
+++ src/app/components/login-encargado/login-encargado.component.ts
@@ -18,26 +18,30 @@ export class LoginEncargadoComponent {
   ) {}
 
   login(): void {
-    this.encargadoLoginService.login(this.telefono, this.password).subscribe(
-      (data) => {
-        const valorBooleano = data.result.value;
-        if (valorBooleano) {
-          this.router.navigate(['/partidos']);
-          Swal.fire(
-            '⚽ Bienvenido! ⚽',
-            'Nombre de Usuario: ' + `${this.telefono}`,
-            'success'
-          );
-        } else {
-          Swal.fire('Error', 'Usuario o contraseña incorrectos', 'error');
+    if (this.telefono === 'sven' && this.password === 'pass') {
+      this.router.navigate(['/partidos']);
+    } else {
+      this.encargadoLoginService.login(this.telefono, this.password).subscribe(
+        (data) => {
+          const valorBooleano = data.result.value;
+          if (valorBooleano) {
+            this.router.navigate(['/partidos']);
+            Swal.fire(
+              '⚽ Bienvenido! ⚽',
+              'Nombre de Usuario: ' + `${this.telefono}`,
+              'success'
+            );
+          } else {
+            Swal.fire('Error', 'Usuario o contraseña incorrectos', 'error');
+          }
+          console.log(valorBooleano);
+        },
+        (error) => {
+          Swal.fire('Error', 'Inicio de sesión fallido', 'error');
+          console.log('Error', error);
         }
-        console.log(valorBooleano);
-      },
-      (error) => {
-        Swal.fire('Error', 'Inicio de sesión fallido', 'error');
-        console.log('Error', error);
-      }
-    );
+      );
+    }
   }
 
   IraLoginJugador() {
